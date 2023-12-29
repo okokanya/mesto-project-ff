d3e55a19c7578a18495ab174554e3186e73df9d7
anya pushkina <okokanya@gmail.com>
2023-12-29 09:44:23 +0800

    all

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,23 @@
-// @todo: Темплейт карточки
+const placesList = document.querySelector('.places__list');
+function cardCreate (cardItems) {
+  cardItems.forEach((cardItem) => {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    cardElement.querySelector('.card__title').textContent = cardItem.name;
+    cardElement.querySelector('.card__image').src = cardItem.link;
+    cardElement.querySelector('.card__image').alt = cardItem.name;
+    const cardDeleteButton = cardElement.querySelector('.card__delete-button');
+    cardDeleteButton.addEventListener('click', function (event) {
+      const parent = (event.target).parentNode;
+      cardRemove(parent)
+    })
+    placesList.append(cardElement);
+    console.log(cardItem);
+  })
+}
 
-// @todo: DOM узлы
+function cardRemove(cardItem) {
+  cardItem.style = "display: none";
+}
 
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+cardCreate(initialCards);
